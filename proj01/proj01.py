729113afd81413a683bb8189bc5391b34699e043
Peabody <peabody@dhcp-129-59-214-80.n1.vanderbilt.edu>
2017-06-12 11:43:16 -0500

    proj01

--- proj01/proj01.py
+++ proj01/proj01.py
@@ -8,3 +8,17 @@
 # If you complete extensions, describe your extensions here!
 
 
+User_input1 = raw_input("Enter your name:")
+
+User_input2 = raw_input("Enter your age:")
+User_input3 = raw_input("Have you had your birthday yet? Y/N:")
+if User_input3 == "Y":
+    My_str4= 101 - int(User_input2)
+else:
+    My_str4 = 100 - int(User_input2)
+My_str = "will be 100 years old in"
+My_str2= "years! Which is the year"
+My_str3= "so you better start living your life."
+print User_input1, My_str, 100- int(User_input2), My_str2, 2017 + int(My_str4), My_str3
+
+
