11ada4bc6f2b8e3fceeb23f4bb3ad94783a7681e
Peabody <peabody@dhcp-129-59-214-80.n1.vanderbilt.edu>
2017-06-13 10:45:18 -0500

    project04.py

--- proj04/proj04.py
+++ proj04/proj04.py
@@ -8,3 +8,13 @@ Asks the user for a string and prints out whether or not the string is a palindr
 
 """
 
+
+User_input1 = raw_input("Type in a word and il see if its a pannagram: ")
+print User_input1 [::-1]
+if User_input1 == User_input1 [::-1]:
+    print "It is"
+else: print "no its not"
+
+
+    #int(len(User_input1)) -= 1
+#while len(word: word[0]==word[-1] word[1:-1}
